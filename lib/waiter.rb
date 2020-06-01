305248773e2cb6cd51a7595673eb59a3c8b5e2ef
Luke Morrison <lukejamesmorrison@gmail.com>
2020-06-01 12:42:18 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,7 +1,14 @@
 class Waiter
 
+  @@all = []
+
   def initialize(name, years_of_experience)
     @name = name
     @experience = years_of_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
